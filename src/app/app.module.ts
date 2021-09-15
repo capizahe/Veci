c26fa62e010031281c30171c2ae4a54cb3618309
Camilo piza <camiloandrespiza@gmail.com>
2021-09-14 21:47:49 -0500

    Cordova packages have been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,19 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers, Storage } from '@ionic/storage';
+
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot(
+    {
+      storeName: '_veci_local',
+      driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
+    }
+  )],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
